878a1a79527b0b03468ab0be1e1eda332b6e6214
Paul Severance <paulseverance@gmail.com>
2017-02-07 17:59:25 -0500

    update tornado-json version

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
         'tornado-json==0.0.1'
     ],
     dependency_links=[
-        'git+https://github.com/sugarush/tornado-json.git@d881e94129056e9e27d188d0d189f52104f139b6#egg=tornado-json-0.0.1'
+        'git+https://github.com/sugarush/tornado-json.git@963b3b3f47c395042c8a59af5956b02321a1374f#egg=tornado-json-0.0.1'
     ]
 )
