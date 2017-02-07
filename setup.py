d33b7ae454e4dde7270bf98397597b22a144cc04
Paul Severance <paulseverance@gmail.com>
2017-02-07 11:23:06 -0500

    update dependencies

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ setup(
     author_email='github@napalmbrain.sugarush.io',
     url='https://github.com/sugarush/tornado-jwt',
     packages=['tornado_jwt'],
-    description='An asynchronous JWT authentication for Tornado.',
+    description='An asynchronous JWT authentication module for Tornado.',
     install_requires=[
         'tornado',
-        'tornado-json'
+        'tornado-json==0.0.1'
     ],
     dependency_links=[
-        'git+https://github.com/sugarush/tornado-json.git@93adcf9e29e3f99c44d93d71292df63ceb5b7df7#egg=tornado-json'
+        'git+https://github.com/sugarush/tornado-json.git@93adcf9e29e3f99c44d93d71292df63ceb5b7df7#egg=tornado-json-0.0.1'
     ]
 )
